c00b447be61696c69efec527776d58baa412d56a
Lucas Morais <152338304+Lucasembare@users.noreply.github.com>
2023-12-13 07:52:12 -0300

    Create index.php
    tentando

---
+++ op_and/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 20;
+$b  = 5;
+$c = 8;
+$d = 4;
+
+
+if ($a > $b && $d > $c ){ // true e false
+    echo "Entrou no primeiro If 1 <br  >";
+}
+
+if ($a > $b && $c   > $d ){ // true e true
+    echo "Entrou no primeiro If 1 <br>";
+}
+
+if (20 > 2 && 3 > 8){ // true e false
+    echo "Entou no 3 If <br>";
+}
+
+
+
+
+?>
