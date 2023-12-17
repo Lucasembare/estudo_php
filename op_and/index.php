4848d300ad1caee6f4691c2f3e8165028e6e8c27
Lucas Morais <152338304+Lucasembare@users.noreply.github.com>
2023-12-17 20:48:50 -0300

    Update index.php
    continuo o estudo

--- op_and/index.php
+++ op_and/index.php
@@ -18,7 +18,25 @@ if (20 > 2 && 3 > 8){ // true e false
     echo "Entou no 3 If <br>";
 }
 
+if (1 > 2 && 3 > 8){ // false e false
+    echo "Entou no 3 If <br>";
+}
 
+if ( $a > $d && $c > $b){ // true e true
+    echo "Entrou no ultimo if <br>";
+}
+
+if ( $a >= $c && $d <= $d){
+    echo "Entro no if 6 <br>";
+} 
+
+if ( $a === $b && $d > $b){
+    echo "If falso";
+}
+
+if (($a >= $b && $c > $d) && $a > $c){
+    echo "Comparação com mais de um AND <br>";
+}
 
 
 ?>
