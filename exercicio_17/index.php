23987b46a973e03286daf006a07476adb98af597
Lucas Morais <152338304+Lucasembare@users.noreply.github.com>
2023-12-17 20:53:54 -0300

    Create index.php
    só um exercicio

---
+++ exercicio_17/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$num = 5;
+
+if (15 > $num && "João" === "João"){
+    echo "Sucesso 1<br>";
+} 
+
+if ("teste" > 5 && 1){
+    echo "Sucesso 2 <br>";
+} 
+
+if ( 2 == 3 && $num >= 3){
+    echo "Sucesso 3 <br>";
+}
+
+
+
+
+
+
+
+?>
