833483a29052feae5a1769f05a31b58914612eef
Lucas Morais <152338304+Lucasembare@users.noreply.github.com>
2023-12-24 09:09:15 -0300

    Aprendendo
    conhecendo os operadores

--- exercicio_18/index.php
+++ exercicio_18/index.php
@@ -1,2 +1,22 @@
 <?php
 
+$a =  12;
+$b = 5;
+$c =20;
+$nome = "João";
+
+if ( $a < $b || $nome === $nome){
+    echo "Verdade 1 <br>";
+}
+
+if ( 1 > $b  || 1) {
+    echo "E verdadeiro 2<br>";
+}
+
+if ( $a === "20" && 51 >= 31){
+    echo "E verdadeiro 3 <br>";
+}
+
+if ( $a == "20" && 51 >= 31){
+    echo "E verdadeiro 4 <br>";
+}
---
+++ op_not/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if (4 > 2){
+    echo "A operação e verdadeira 1 <br>";
+}
+
+if (!( 10 < 9 )){
+    echo "A operação e verdsdeira  2 <br>";
+}
+
+if (!( "Lucas" === "Pedro")){
+    echo "Não são identicos <br>";
+}
