3ce3977f0d167b6c1ea63f60860a1c57e50e9d21
Lucas Morais <152338304+Lucasembare@users.noreply.github.com>
2023-12-27 07:52:14 -0300

    Criação de nova index
    cast em variavel

---
+++ op_cast/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = (int) "12";
+
+
+echo $a . "<br>";
+
+echo $a + 10 . "<br>";
+
+if ($a === 12){
+    echo "A e identico a 12 <br>";
+}
+
+$b = (float) "3.454";
+
+if ($b === 3.454){
+    echo "B é identico a B <br>";
+}
+
+$c = (string) 34;
+
+echo $c . "<br>";  
+
+if ($c === "34"){
+    echo "C e identico a 34 <br>";
+}
